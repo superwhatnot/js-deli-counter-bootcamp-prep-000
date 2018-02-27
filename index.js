3e01231eb38439662182f503b9bc2379d76dd30e
Raja H <rajahamid12.7@gmail.com>
2018-02-27 02:14:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+var katzDeli = [];
+
+function takeANumber(katzDeli, name) {
+  katzDeli.push(name)
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
+}
+
+function nowServing(katzDeli) {
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+  else {
+    return `Currently serving ${katzDeli.shift()}.`
+  }
+}
+
+/*
+For example, if katzDeliLine is currently ["Ada", "Grace"],
+currentLine(katzDeliLine) would return
+"The line is currently: 1. Ada, 2. Grace".
+>>> string + while loop (number + reading the array)
+*/
+
+function currentLine(katzDeli) {
+  if (katzDeli.length === 0) {
+    return `The line is currently empty.`
+  }
+  else {
+    return `The line is currently not empty`
+  }
+}
